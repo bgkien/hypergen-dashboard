3e082340a2b2b529088ddfbdf3c0e42ffc5b4a17
kien-hypergenio <kien@hypergen.io>
2024-12-06 06:47:59 +0200

    fix: update build configuration for Vercel
    - Update buildCommand in vercel.json
    - Add base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 6173, // Changed from default 5173
   },
